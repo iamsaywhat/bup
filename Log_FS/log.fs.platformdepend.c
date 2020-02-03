4e654a718d8826800e7383a67a4024f75b7276af
iamsaywhat <7a653s9@mail.ru>
2020-02-03 15:47:52 +0300

    Все в порядке с обновленной файловой системой, забыл платфомозависимую часть перенести в новые файлы

--- Log_FS/log.fs.platformdepend.c
+++ Log_FS/log.fs.platformdepend.c
@@ -1,26 +1,29 @@
 #include "log.fs.platformdepend.h"
-
+#include "25q64fv.h"
 
 /******************************************************************************************
-*                         Приватные функции нижнего уровня                                *
+*                         РџСЂРёРІР°С‚РЅС‹Рµ С„СѓРЅРєС†РёРё РЅРёР¶РЅРµРіРѕ СѓСЂРѕРІРЅСЏ                                *
 ******************************************************************************************/
 void writeMemory(uint32_t Address, uint8_t* buffer, uint32_t size)
 {
-
+  for(uint32_t i = 0; i < size; i++)
+  {
+    SPI_25Q64FV_writeByte(SPI_25Q64FV_CSn, Address+i, buffer[i]);
+  }
 }
 
 
 void readMemory(uint32_t Address, uint8_t* buffer, uint32_t size)
 {
-
+  SPI_25Q64FV_readArray (SPI_25Q64FV_CSn, Address, buffer, size);
 }
 
 void eraseChip(void)
 {
-
+  SPI_25Q64FV_eraseChip(SPI_25Q64FV_CSn);
 }
 
 void eraseSector(uint32_t Address)
 {
-
+  SPI_25Q64FV_eraseSector (SPI_25Q64FV_CSn, Address);
 }
