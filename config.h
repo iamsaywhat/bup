0372f5646522db5366f2e4b358a0c5e91f5496f0
Andrey Seliverstov <7a653s9@mail.ru>
2020-07-03 12:37:24 +0300

    Учет версий вынесен в config.h

--- config.h
+++ config.h
@@ -5,7 +5,23 @@
 #define _CONFIG_H_
 
 //#define flightRegulatorCFB       // Использовать flightRegulatorCFB регулятор
-#define DEBUG_VARS               // Включить вывод отладочной информации (модуль debug.c)
-#define LOGS_ENABLE              // Включить логирование в черный ящик
+#define DEBUG_VARS               // Включить модуль отладки (модуль debug.c)
+#define LOGS_ENABLE              // Включить модуль логирования в черный ящик
+
+
+/* Учет версий ПО */
+#define FIRMWARE_VERSION_MAJOR    0     // Старшая версия ПО
+#define FIRMWARE_VERSION_MINOR    32    // Младшая версия ПО
+#define FIRMWARE_VERSION_MICRO    3     // Изменения
+
+#ifdef flightRegulatorCFB
+  #define MATH_OPTION             1     // Опция регулятора  
+  #define MATH_VERSION_MAJOR      0     // Cтаршая версия регулятора
+  #define MATH_VERSION_MINOR      1     // Младшая версия регулятора
+#else
+  #define MATH_OPTION             2     // Опция регулятора  
+  #define MATH_VERSION_MAJOR      1     // Cтаршая версия регулятора
+  #define MATH_VERSION_MINOR      0     // Младшая версия регулятора
+#endif
 
 #endif
